9b2a3e89fcec96d83127705b9f57a9b5ba96bbc5
Vladick102 <sydorak.pn@ucu.edu.ua>
2024-10-08 16:02:38 +0300

    Added WordCloud

--- visualisation.R
+++ visualisation.R
@@ -9,7 +9,8 @@ library(readr)
 library(ggplot2)
 library(tidyr)
 library(patchwork)
-
+library(RColorBrewer)
+library(wordcloud)
 
 stop_words <- read_file("stop_words.txt")
 # https://stackoverflow.com/questions/27195912/why-does-strsplit-return-a-list
@@ -21,9 +22,9 @@ test <- read.csv(file = test_path, stringsAsFactors = FALSE)
 
 
 create_bag_of_words <- function(data, stop_words) {
-  # Видаляємо пунктуацію та цифри
+
   data$Message <- str_replace_all(data$Message, "[[:punct:]]", " ")
-  data$Message <- str_replace_all(data$Message, "[[:digit:]]", " ") # Видаляємо цифри
+  data$Message <- str_replace_all(data$Message, "[[:digit:]]", " ")
   data$Message <- str_to_lower(data$Message)
 
   data$Category <- ifelse(data$Category == "ham", 0, 1)
@@ -53,11 +54,6 @@ create_bag_of_words <- function(data, stop_words) {
   return(list(bag_of_words_matrix = dtm_df, labels = labels, word_frequency = word_freq))
 }
 
-result <- create_bag_of_words(train, splitted_stop_words)
-
-
-
-
 plot_top_5 <- function(word_freq) {
   top_spam <- word_freq %>%
     arrange(desc(spam)) %>%
@@ -67,7 +63,6 @@ plot_top_5 <- function(word_freq) {
     arrange(desc(ham)) %>%
     slice_head(n = 5)
 
-  # Plot histogram for Ham
   p1 <- ggplot(top_ham, aes(x = word, y = ham, fill = "Ham")) +
     geom_bar(stat = "identity") +
     labs(
@@ -78,7 +73,6 @@ plot_top_5 <- function(word_freq) {
     scale_fill_manual(values = c("Ham" = "#6987dc")) +
     theme(legend.position = "none")
 
-  # Plot histogram for Spam
   p2 <- ggplot(top_spam, aes(x = word, y = spam, fill = "Spam")) +
     geom_bar(stat = "identity") +
     labs(
@@ -94,6 +88,38 @@ plot_top_5 <- function(word_freq) {
   print(combined_plot)
 }
 
+plot_word_cloud <- function(word_freq) {
 
-word_frequency <- result$word_frequency
+  ham_words <- subset(word_freq, ham > 0)
+  spam_words <- subset(word_freq, spam > 0)
+  
+  par(mfrow = c(1, 2))
+
+  c1 <- wordcloud(
+    words = ham_words$word,
+    freq = ham_words$ham,
+    min.freq = 1,
+    max.words = 80,
+    colors = brewer.pal(8, "Blues"),
+    random.order = FALSE
+  )
+  
+  c2 <- wordcloud(
+    words = spam_words$word,
+    freq = spam_words$spam,
+    min.freq = 1,
+    max.words = 80,
+    colors = brewer.pal(8, "Reds"),
+    random.order = FALSE
+  )
+  
+  par(mfrow = c(1, 1))
+}
+  
+
+result <- create_bag_of_words(train, splitted_stop_words)
+word_freq <- result$word_frequency
+
 plot_top_5(word_frequency)
+plot_word_cloud(word_frequency)
+
